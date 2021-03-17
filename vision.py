52eed748aa5b437d7f19dab2b3396ef5ba6c58c9
Jonah <jonahkohn@gmail.com>
2021-03-17 07:35:23 -0700

    Added lucent

--- vision.py
+++ vision.py
@@ -2,13 +2,14 @@ import torch
 import lucent
 from torchvision.models import vgg16
 from lucent.optvis import render, param, transform
+from lucent.modelzoo import util
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = vgg16(pretrained=True)
 
-lucent.lucent.modelzoo.util.get_model_layers(model)
+print(util.get_model_layers(model))
 
 model.to(device).eval()
 
-obj = "layer2:9" # a ResNet50 layer and channel
+obj = "features_22:8" # a ResNet50 layer and channel
 render.render_vis(model, obj)
