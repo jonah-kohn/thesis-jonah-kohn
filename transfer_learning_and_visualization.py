79d0b1b93a01b170b577eadb8865b6c9f74824dd
Jonah <jonahkohn@gmail.com>
2021-03-16 19:31:55 -0700

    Fix image augmentations

--- transfer_learning_and_visualization.py
+++ transfer_learning_and_visualization.py
@@ -27,12 +27,11 @@ batch_size = 128
 image_transform = transforms.Compose([
         transforms.Resize(size=256),
         transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomVerticalFlip(p=0.5),
-        transforms.RandomRotation(180),
-        transforms.RandomRotation(90)
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+
     ])
 
 data = {
