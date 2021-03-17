51477686ca06a316bc4951fa93b1d6e8a40fc39b
Jonah <jonahkohn@gmail.com>
2021-03-17 03:00:40 -0700

    GradCam test

--- transfer_learning_and_visualization.py
+++ transfer_learning_and_visualization.py
@@ -236,7 +236,7 @@ def train(model,
 
 # model = get_pretrained_model()
 
-model, history = train(
+model, _ = train(
     model,
     criterion,
     optimizer,
@@ -247,32 +247,160 @@ model, history = train(
     n_epochs=30
     )
 
-# #History plots
-# plt.figure(figsize=(8, 6))
-# for c in ['train_loss', 'valid_loss']:
-#     plt.plot(
-#         history[c], label=c)
-# plt.legend()
-# plt.xlabel('Epoch')
-# plt.ylabel('Negative Log')
-# plt.title('Loss')
-#
-# plt.figure(figsize=(8, 6))
-# for c in ['train_acc', 'valid_acc']:
-#     plt.plot(
-#         100 * history[c], label=c)
-# plt.legend()
-# plt.xlabel('Epoch')
-# plt.ylabel('Average Acc')
-# plt.title('Accuracy')
-#
-# model, history = train(
-#     model,
-#     criterion,
-#     optimizer,
-#     dataloaders['train'],
-#     dataloaders['val'],
-#     save_file_name=save_file_name,
-#     max_epochs_stop=5,
-#     n_epochs=30,
-#     print_every=2)
+
+def get_example_params(example_index, model):
+    """
+        Gets used variables for almost all visualizations, like the image, model etc.
+    Args:
+        example_index (int): Image id to use from examples
+    returns:
+        original_image (numpy arr): Original image read from the file
+        prep_img (numpy_arr): Processed image
+        target_class (int): Target class for the image
+        file_name_to_export (string): File name to export the visualizations
+        pretrained_model(Pytorch model): Model to use for the operations
+    """
+    # Pick one of the examples
+    example_list = ((traindir + 'ModerateDemented/mildDem0.jpg', 1),
+                    (traindir + 'NonDemented/nonDem0.jpg', 0))
+    img_path = example_list[example_index][0]
+    target_class = example_list[example_index][1]
+    file_name_to_export = img_path[img_path.rfind('/')+1:img_path.rfind('.')]
+    # Read image
+    original_image = Image.open(img_path).convert('RGB')
+    # Process image
+    prep_img = preprocess_image(original_image)
+    # Define model
+    pretrained_model = model
+    return (original_image,
+            prep_img,
+            target_class,
+            file_name_to_export,
+            pretrained_model)
+
+def save_class_activation_images(org_img, activation_map, file_name):
+    """
+        Saves cam activation map and activation map on the original image
+    Args:
+        org_img (PIL img): Original image
+        activation_map (numpy arr): Activation map (grayscale) 0-255
+        file_name (str): File name of the exported image
+    """
+    if not os.path.exists('../results'):
+        os.makedirs('../results')
+    # Grayscale activation map
+    heatmap, heatmap_on_image = apply_colormap_on_image(org_img, activation_map, 'hsv')
+    # Save colored heatmap
+    path_to_file = os.path.join('../results', file_name+'_Cam_Heatmap.png')
+    save_image(heatmap, path_to_file)
+    # Save heatmap on iamge
+    path_to_file = os.path.join('../results', file_name+'_Cam_On_Image.png')
+    save_image(heatmap_on_image, path_to_file)
+    # SAve grayscale heatmap
+    path_to_file = os.path.join('../results', file_name+'_Cam_Grayscale.png')
+    save_image(activation_map, path_to_file)
+
+
+from misc_functions import get_example_params, save_class_activation_images
+
+
+class CamExtractor():
+    """
+        Extracts cam features from the model
+    """
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+        self.gradients = None
+
+    def save_gradient(self, grad):
+        self.gradients = grad
+
+    def forward_pass_on_convolutions(self, x):
+        """
+            Does a forward pass on convolutions, hooks the function at given layer
+        """
+        conv_output = None
+        for module_pos, module in self.model.features._modules.items():
+            x = module(x)  # Forward
+            if int(module_pos) == self.target_layer:
+                x.register_hook(self.save_gradient)
+                conv_output = x  # Save the convolution output on that layer
+        return conv_output, x
+
+    def forward_pass(self, x):
+        """
+            Does a full forward pass on the model
+        """
+        # Forward pass on the convolutions
+        conv_output, x = self.forward_pass_on_convolutions(x)
+        x = x.view(x.size(0), -1)  # Flatten
+        # Forward pass on the classifier
+        x = self.model.classifier(x)
+        return conv_output, x
+
+
+class GradCam():
+    """
+        Produces class activation map
+    """
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.model.eval()
+        # Define extractor
+        self.extractor = CamExtractor(self.model, target_layer)
+
+    def generate_cam(self, input_image, target_class=None):
+        # Full forward pass
+        # conv_output is the output of convolutions at specified layer
+        # model_output is the final output of the model (1, 1000)
+        conv_output, model_output = self.extractor.forward_pass(input_image)
+        if target_class is None:
+            target_class = np.argmax(model_output.data.numpy())
+        # Target for backprop
+        one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
+        one_hot_output[0][target_class] = 1
+        # Zero grads
+        self.model.features.zero_grad()
+        self.model.classifier.zero_grad()
+        # Backward pass with specified target
+        model_output.backward(gradient=one_hot_output, retain_graph=True)
+        # Get hooked gradients
+        guided_gradients = self.extractor.gradients.data.numpy()[0]
+        # Get convolution outputs
+        target = conv_output.data.numpy()[0]
+        # Get weights from gradients
+        weights = np.mean(guided_gradients, axis=(1, 2))  # Take averages for each gradient
+        # Create empty numpy array for cam
+        cam = np.ones(target.shape[1:], dtype=np.float32)
+        # Multiply each weight with its conv output and then, sum
+        for i, w in enumerate(weights):
+            cam += w * target[i, :, :]
+        cam = np.maximum(cam, 0)
+        cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
+        cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
+        cam = np.uint8(Image.fromarray(cam).resize((input_image.shape[2],
+                       input_image.shape[3]), Image.ANTIALIAS))/255
+        # ^ I am extremely unhappy with this line. Originally resizing was done in cv2 which
+        # supports resizing numpy matrices with antialiasing, however,
+        # when I moved the repository to PIL, this option was out of the window.
+        # So, in order to use resizing with ANTIALIAS feature of PIL,
+        # I briefly convert matrix to PIL image and then back.
+        # If there is a more beautiful way, do not hesitate to send a PR.
+
+        # You can also use the code below instead of the code line above, suggested by @ ptschandl
+        # from scipy.ndimage.interpolation import zoom
+        # cam = zoom(cam, np.array(input_image[0].shape[1:])/np.array(cam.shape))
+        return cam
+
+
+target_example = 0  # Snake
+(original_image, prep_img, target_class, file_name_to_export, pretrained_model) =\
+    get_example_params(target_example)
+# Grad cam
+grad_cam = GradCam(pretrained_model, target_layer=11)
+# Generate cam mask
+cam = grad_cam.generate_cam(prep_img, target_class)
+# Save mask
+save_class_activation_images(original_image, cam, file_name_to_export)
+print('Grad cam completed')
