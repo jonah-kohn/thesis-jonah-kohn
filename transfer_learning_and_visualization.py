df32a343fc13204cfee329f1305a4fceeb826224
Jonah <jonahkohn@gmail.com>
2021-03-17 03:36:14 -0700

    add fcn

--- transfer_learning_and_visualization.py
+++ transfer_learning_and_visualization.py
@@ -3,11 +3,13 @@ from torchvision import transforms, datasets, models
 import torch
 from torch import optim, cuda
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 import torch.nn as nn
 
 import numpy as np
 import pandas as pd
 import os
+
 
 from PIL import Image
 
@@ -268,7 +270,9 @@ def get_example_params(example_index):
     file_name_to_export = img_path[img_path.rfind('/')+1:img_path.rfind('.')]
     # Read image
     original_image = Image.open(img_path).convert('RGB')
-
+    # Process image
+    prep_img = preprocess_image(original_image)
+    # Define model
     pretrained_model = model
     return (original_image,
             prep_img,
@@ -332,6 +336,46 @@ class CamExtractor():
         # Forward pass on the classifier
         x = self.model.classifier(x)
         return conv_output, x
+
+
+def preprocess_image(pil_im, resize_im=True):
+    """
+        Processes image for CNNs
+    Args:
+        PIL_img (PIL_img): PIL Image or numpy array to process
+        resize_im (bool): Resize to 224 or not
+    returns:
+        im_as_var (torch variable): Variable that contains processed float tensor
+    """
+    # mean and std list for channels (Imagenet)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    #ensure or transform incoming image to PIL image
+    if type(pil_im) != Image.Image:
+        try:
+            pil_im = Image.fromarray(pil_im)
+        except Exception as e:
+            print("could not transform PIL_img to a PIL Image object. Please check input.")
+
+    # Resize image
+    if resize_im:
+        pil_im = pil_im.resize((224, 224), Image.ANTIALIAS)
+
+    im_as_arr = np.float32(pil_im)
+    im_as_arr = im_as_arr.transpose(2, 0, 1)  # Convert array to D,W,H
+    # Normalize the channels
+    for channel, _ in enumerate(im_as_arr):
+        im_as_arr[channel] /= 255
+        im_as_arr[channel] -= mean[channel]
+        im_as_arr[channel] /= std[channel]
+    # Convert to float tensor
+    im_as_ten = torch.from_numpy(im_as_arr).float()
+    # Add one more channel to the beginning. Tensor shape = 1,3,224,224
+    im_as_ten.unsqueeze_(0)
+    # Convert to Pytorch variable
+    im_as_var = Variable(im_as_ten, requires_grad=True)
+    return im_as_var
 
 
 class GradCam():
